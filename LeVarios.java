5674c38a264015985f73f39abb238d208374943b
RobsonCodeIT <57295055+RobsonCodeIT@users.noreply.github.com>
2020-04-02 14:43:01 -0300

    Create LeVarios.java
    Leitura de vários dados e mostrando na mesma linha. Aula 24.

---
+++ LeVarios.java
@@ -0,0 +1,22 @@
+import  java.util.Scanner;
+import  java.util.Locale;
+
+public class LeVarios{
+  public static coid main(String[] args) {
+    locale.setDefault(Locale.US);
+    
+    Scanner sc = new Scanner(System.in);
+    
+    String  x;
+    int     y;
+    double  z;
+    
+    System.out.println("Dandos digitados: ");
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+    
+    sc.close();
+    
+  }
+}
