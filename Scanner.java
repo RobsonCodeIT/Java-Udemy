5735b34e1962b891a1cfc46cfdbe5af012ceb47b
RobsonCodeIT <57295055+RobsonCodeIT@users.noreply.github.com>
2020-04-02 12:44:18 -0300

    Update Scanner.java
    Lê um Double e mostra na Saída. Pega o formato de saída do local (Locale.US). Aula 24

--- Scanner.java
+++ Scanner.java
@@ -5,12 +5,17 @@ publicc class Scanner {
     Scanner sc = new Scanner(System.in); //inicializa a variável sc
     
     String x;
+    int y;
+    Double z;
     
     x = sc.next(); // Lê uma palavra
     System.out.println("Voce digitou: " + x);    
     
-    x = sc.nextInt(); //Lê um inteiro
-    System.out.println("Voce digitou: " + x);    
+    y = sc.nextInt(); //Lê um inteiro
+    System.out.println("Voce digitou: " + y);     
+
+    z = sc.nextDouble(); //Lê um Double. Pega a formação da llocalidade (Locale.US)
+    System.out.println("Voce digitou: " + z);   
     
     sc.close(); //encerra a variável sc
   }
