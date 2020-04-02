f2e1795122f62da419829d86fd94bf7792c6767c
RobsonCodeIT <57295055+RobsonCodeIT@users.noreply.github.com>
2020-04-02 12:36:34 -0300

    Update Scanner.java
    Lê um inteiro e mostra na saída. Aula 24.

--- Scanner.java
+++ Scanner.java
@@ -6,8 +6,10 @@ publicc class Scanner {
     
     String x;
     
-    x = sc.next();
+    x = sc.next(); // Lê uma palavra
+    System.out.println("Voce digitou: " + x);    
     
+    x = sc.nextInt(); //Lê um inteiro
     System.out.println("Voce digitou: " + x);    
     
     sc.close(); //encerra a variável sc
