4a8ece6dc58d5c4a49cc99b8615f414258d986b1
RobsonCodeIT <57295055+RobsonCodeIT@users.noreply.github.com>
2020-04-02 14:31:41 -0300

    Update Scanner.java
    Exemplo de entrada de dados com charAT.

--- Scanner.java
+++ Scanner.java
@@ -6,9 +6,10 @@ publicc class Scanner {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in); //inicializa a variável sc
     
-    String x;
-    int y;
-    Double z;
+    String  x;
+    int     y;
+    Double  z;
+    char    a;
     
     x = sc.next(); // Lê uma palavra
     System.out.println("Voce digitou: " + x);    
@@ -19,6 +20,9 @@ publicc class Scanner {
     z = sc.nextDouble(); //Lê um Double. Pega a formação da llocalidade (Locale.US)
     System.out.println("Voce digitou: " + z);   
     System.out.printf("Voce digitou: %.2f%n", z); //print formatado
+    
+    a = sc.next().charAt(0); //O valor em charAT indica a posição do caractere que se deseja capturar
+    System.out.println("Voce digitou: " + a);
     
     sc.close(); //encerra a variável sc
   }
