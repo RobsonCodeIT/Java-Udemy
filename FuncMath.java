b942d519c8ffdab9e45487086cdcd7963cbd5cb9
RobsonCodeIT <57295055+RobsonCodeIT@users.noreply.github.com>
2020-04-03 15:03:22 -0300

    Update FuncMath.java
    Pequenas correções, erros de digitação. Aula 26.

--- FuncMath.java
+++ FuncMath.java
@@ -1,8 +1,8 @@
 public class FuncMath {
   public static void main(String[] args) {
     double  x = 3.0;
-    double  y = 4.0
-    double  z = -5.0
+    double  y = 4.0;
+    double  z = -5.0;
     double  A, B, C;
     
     A = Math.sqrt(x);
@@ -10,7 +10,7 @@ public class FuncMath {
     C = Math.sqrt(25.0);
     
     System.out.println("Raiz quadrada de " + x + " = " + A);
-    System.out.println("Raiz quadrada de " + x + " = " + B);
+    System.out.println("Raiz quadrada de " + y + " = " + B);
     System.out.println("Raiz quadrada de 25 = " + C);
     
     A = Math.pow(x, y);
